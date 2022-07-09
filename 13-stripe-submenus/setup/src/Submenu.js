27620b964385c9b5b047e48162833e860ea9fac0
Tim Romanowski <timromanowski15@gmail.com>
2022-07-09 19:55:36 +0200

    Finish Stripe project

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -1,7 +1,46 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react';
+import { useGlobalContext } from './context';
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
-}
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
+  const container = useRef(null);
+  const [colums, setColumns] = useState('col-2');
+  useEffect(() => {
+    setColumns('col-2');
+    const submenu = container.current;
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+    if (links.length === 3) {
+      setColumns('col-3');
+    }
+    if (links.length === 4) {
+      setColumns('col-4');
+    }
+  }, [location, links]);
+  return (
+    <aside
+      ref={container}
+      className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
+    >
+      <h4>{page}</h4>
+      <div className={`submenu-center ${colums}`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link;
+          return (
+            <a key={index} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </aside>
+  );
+};
 
-export default Submenu
+export default Submenu;
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -4,8 +4,10 @@ import sublinks from './data';
 const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [location, setLocation] = useState({});
+  const [page, setPage] = useState({ page: '', links: [] });
 
   const openSidebar = () => {
     setIsSidebarOpen(true);
@@ -13,7 +15,10 @@ export const AppProvider = ({ children }) => {
   const closeSidebar = () => {
     setIsSidebarOpen(false);
   };
-  const openSubmenu = () => {
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
     setIsSubmenuOpen(true);
   };
   const closeSubmenu = () => {
@@ -29,6 +34,8 @@ export const AppProvider = ({ children }) => {
         openSidebar,
         closeSidebar,
         closeSubmenu,
+        location,
+        page,
       }}
     >
       {children}
