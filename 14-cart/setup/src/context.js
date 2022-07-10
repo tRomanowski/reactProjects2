803447b0448316dc8e5c407c74933df0532db0ac
Tim Romanowski <timromanowski15@gmail.com>
2022-07-10 18:29:42 +0200

    Add increase and decrease amount

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -24,12 +24,21 @@ const AppProvider = ({ children }) => {
     dispatch({ type: 'REMOVE', payload: id });
   };
 
+  const increase = (id) => {
+    dispatch({ type: 'INCREASE', payload: id });
+  };
+  const decrease = (id) => {
+    dispatch({ type: 'DECREASE', payload: id });
+  };
+
   return (
     <AppContext.Provider
       value={{
         ...state,
         clearCart,
         remove,
+        increase,
+        decrease,
       }}
     >
       {children}
