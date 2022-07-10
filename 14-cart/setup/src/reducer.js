cd7f11141dcab8e02801548147b099294a6342c3
Tim Romanowski <timromanowski15@gmail.com>
2022-07-10 19:26:50 +0200

    Add Error

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -75,7 +75,7 @@ const reducer = (state, action) => {
   if (action.type === 'DISPLAY_ITEMS') {
     return { ...state, cart: action.payload, loading: false };
   }
-  return state;
+  throw new Error('no mathing action type');
 };
 
 export default reducer;
