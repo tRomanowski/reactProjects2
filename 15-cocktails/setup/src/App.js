612155d3ea16cbf262a2a34a6d90ab38a18dfdf4
Tim Romanowski <timromanowski15@gmail.com>
2022-07-19 18:55:58 +0200

    Finish project

--- 15-cocktails/setup/src/App.js
+++ 15-cocktails/setup/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import pages
 import Home from './pages/Home';
 import About from './pages/About';
@@ -12,20 +12,12 @@ function App() {
     <div>
       <Router>
         <Navbar />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/about'>
-            <About />
-          </Route>
-          <Route path='/cocktail/:id'>
-            <SingleCocktail />
-          </Route>
-          <Route path='*'>
-            <Error />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='cocktail/:id' element={<SingleCocktail />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
       </Router>
     </div>
   );
