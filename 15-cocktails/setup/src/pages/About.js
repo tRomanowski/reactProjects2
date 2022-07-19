d4796ed10be189e65a9edd9f515944ff97c25952
Tim Romanowski <timromanowski15@gmail.com>
2022-07-19 13:55:03 +0200

    Setup react-router, about page and error page

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section abozt-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate illo
+        fugiat consectetur cum quaerat reiciendis, vel suscipit eaque iure sint
+        saepe quo veritatis, provident molestias! Ea sit quae et, debitis minima
+        aut, amet corporis magnam odio earum eligendi quam consequatur?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
