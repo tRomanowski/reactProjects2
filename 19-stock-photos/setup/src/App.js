9c42e9a5f386de71ed5831529dd5337ad65a6f6f
Tim Romanowski <timromanowski15@gmail.com>
2022-07-22 16:50:40 +0200

    Add List of fetched photos

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -1,12 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+import React, { useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import Photo from './Photo';
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url = `${mainUrl}${clientID}`;
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setPhotos(data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+  };
+
+  return (
+    <main>
+      <section className='search'>
+        <form className='search-form' onSubmit={handleSubmit}>
+          <input type='text' placeholder='search' className='form-input' />
+          <button className='submit-btn' type='submit'>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((photo) => {
+            return <Photo key={photo.id} {...photo} />;
+          })}
+        </div>
+      </section>
+      {loading && <h2 className='loading'>Loading...</h2>}
+    </main>
+  );
 }
 
-export default App
+export default App;
