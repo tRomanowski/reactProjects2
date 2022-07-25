28314676ea6fe1c690db8a04b97042fde4514e03
Tim Romanowski <timromanowski15@gmail.com>
2022-07-25 14:43:15 +0200

    Finish dark mode project

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -1,9 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from 'react';
+import data from './data';
+import Article from './Article';
+import articles from './data';
+
+const getStorageTheme = () => {
+  let theme = 'light-theme';
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+  return theme;
+};
 
 function App() {
-  return <h2>dark mode starter</h2>
+  const [theme, setTheme] = useState(getStorageTheme());
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  };
+  return (
+    <main>
+      <nav>
+        <div className='nav-center'>
+          <h1>overreacted</h1>
+          <button className='btn' onClick={toggleTheme}>
+            toggle
+          </button>
+        </div>
+        <section>
+          {data.map((item) => {
+            return <Article key={item.id} {...item} />;
+          })}
+        </section>
+      </nav>
+    </main>
+  );
 }
 
-export default App
+export default App;
