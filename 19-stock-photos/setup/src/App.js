8d97313af318beaa2c14df5c97b58c2c4437f349
Tim Romanowski <timromanowski15@gmail.com>
2022-07-25 12:06:49 +0200

    Fix bugs

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import Photo from './Photo';
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
@@ -8,8 +8,10 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
   const [query, setQuery] = useState('');
+  const mounted = useRef(false);
+  const [newImages, setNewImages] = useState(false);
 
   const fetchImages = async () => {
     setLoading(true);
@@ -35,34 +37,48 @@ function App() {
           return [...oldPhotos, ...data];
         }
       });
-      console.log(data);
+      setNewImages(false);
       setLoading(false);
     } catch (error) {
+      setNewImages(false);
       setLoading(false);
       console.log(error);
     }
   };
+
   useEffect(() => {
     fetchImages();
   }, [page]);
 
   useEffect(() => {
-    const event = window.addEventListener('scroll', () => {
-      if (
-        !loading &&
-        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
-      ) {
-        setPage((oldPage) => {
-          return oldPage + 1;
-        });
-      }
+    if (!mounted.current) {
+      mounted.current = true;
+      return;
+    }
+    if (!newImages) return;
+    if (loading) return;
+    setPage((oldPage) => {
+      return oldPage + 1;
     });
+  }, [newImages]);
+
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImages(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', event);
     return () => window.removeEventListener('scroll', event);
   }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setPage(1);
+    if (!query) return;
+    if (page === 1) {
+      fetchImages();
+    }
   };
 
   return (
