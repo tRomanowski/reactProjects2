324e967fedd9c2b1f22c4851770ba39734ec4872
Tim Romanowski <timromanowski15@gmail.com>
2022-07-26 15:12:57 +0200

    Add Search function

--- 22-hacker-news/setup/src/reducer.js
+++ 22-hacker-news/setup/src/reducer.js
@@ -23,6 +23,8 @@ const reducer = (state, action) => {
       });
 
       return { ...state, hits: newHits };
+    case HANDLE_SEARCH:
+      return { ...state, query: action.payload, page: 0 };
     default:
       throw new Error(`no matching "${action.type}" action type`);
   }
