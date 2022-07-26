e8b0d83ffa87b7cf094513f54aa68f2bdf600c5b
Tim Romanowski <timromanowski15@gmail.com>
2022-07-26 18:57:11 +0200

    Finish Hacker News

--- 22-hacker-news/setup/src/Buttons.js
+++ 22-hacker-news/setup/src/Buttons.js
@@ -1,8 +1,22 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from 'react';
+import { useGlobalContext } from './context';
 
 const Buttons = () => {
-  return <h2>button container</h2>
-}
+  const { isLoading, page, nbPages, handlePage } = useGlobalContext();
 
-export default Buttons
+  return (
+    <div className='btn-container'>
+      <button onClick={() => handlePage('dec')} disabled={isLoading}>
+        prev
+      </button>
+      <p>
+        {page + 1} of {nbPages}
+      </p>
+      <button onClick={() => handlePage('inc')} disabled={isLoading}>
+        next
+      </button>
+    </div>
+  );
+};
+
+export default Buttons;
--- 22-hacker-news/setup/src/reducer.js
+++ 22-hacker-news/setup/src/reducer.js
@@ -25,6 +25,21 @@ const reducer = (state, action) => {
       return { ...state, hits: newHits };
     case HANDLE_SEARCH:
       return { ...state, query: action.payload, page: 0 };
+    case HANDLE_PAGE:
+      if (action.payload === 'inc') {
+        let nextPage = state.page + 1;
+        if (nextPage > state.nbPages - 1) {
+          nextPage = 0;
+        }
+        return { ...state, page: nextPage };
+      }
+      if (action.payload === 'dec') {
+        let nextPage = state.page - 1;
+        if (nextPage < 0) {
+          nextPage = state.nbPages - 1;
+        }
+        return { ...state, page: nextPage };
+      }
     default:
       throw new Error(`no matching "${action.type}" action type`);
   }
